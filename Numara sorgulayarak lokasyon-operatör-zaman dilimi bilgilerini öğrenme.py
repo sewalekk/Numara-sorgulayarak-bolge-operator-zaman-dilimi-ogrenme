c8c9170440d032e2717b4db5e0a26b70f131551e
Şevval Ekinci <sevvalekinci6@gmail.com>
2023-01-31 00:11:40 +0300

    Create Numara sorgulayarak lokasyon-operatör-zaman dilimi bilgilerini öğrenme.py

---
+++ Numara sorgulayarak lokasyon-operatör-zaman dilimi bilgilerini öğrenme.py
@@ -0,0 +1,16 @@
+import phonenumbers
+
+number1="+14103333333"
+number2="+905395664088"
+
+from phonenumbers import geocoder
+number=phonenumbers.parse(number1)
+print(geocoder.description_for_number(number,"tr"))
+
+from phonenumbers import carrier
+number=phonenumbers.parse(number2)
+print(carrier.name_for_number(number,"tr"))
+
+from phonenumbers import timezone
+number=phonenumbers.parse(number1)
+print(timezone.time_zones_for_number(number))
